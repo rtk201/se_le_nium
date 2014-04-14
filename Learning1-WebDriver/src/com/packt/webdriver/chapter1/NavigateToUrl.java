017e7e6f0e0c44ff948cdb9225bb1189d6faf79b
Wenna <whayudini@emiva.net>
2014-04-14 15:13:25 -0400

    Present on Google page is made and using list<weblement by tagname

--- Learning1-WebDriver/src/com/packt/webdriver/chapter1/NavigateToUrl.java
+++ Learning1-WebDriver/src/com/packt/webdriver/chapter1/NavigateToUrl.java
@@ -7,9 +7,8 @@ public class NavigateToUrl {
 	public static void main(String[] args){
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
-		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("Packt Publishing");
-		searchBox.submit();
+		java.util.List<WebElement> inputs =  driver.findElements(By.tagName("input")) ;
+		System.out.println(inputs.size());
 	}
 
 }
